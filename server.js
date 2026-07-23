d69cbf015e74c4f325a806a540fe3d88d379193c
Maahirgit <Maahirgit@gmail.com>
2026-07-23 21:03:23 +0530

    Fix Cloudinary configuration to respect system CLOUDINARY_URL if CLOUDINARY_CLOUD_NAME is not present

--- server.js
+++ server.js
@@ -115,11 +115,13 @@ const Reel = mongoose.model('Reel', reelSchema);
 const { v2: cloudinary } = require('cloudinary');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
+if (process.env.CLOUDINARY_CLOUD_NAME) {
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+}
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
