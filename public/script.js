b2ea78d2af32c426b3f5a90738e3c8741a505777
Maahirgit <Maahirgit@gmail.com>
2026-07-25 13:23:08 +0530

    Fix: Add missing phone number formatting to forgot password input

--- public/script.js
+++ public/script.js
@@ -48,10 +48,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Phone number formatting
     const phoneInput = document.getElementById('signup-number');
-    phoneInput.addEventListener('input', function (e) {
+    const forgotPhoneInput = document.getElementById('forgot-number');
+
+    const formatPhone = function (e) {
         let x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,5})(\d{0,5})/);
         e.target.value = !x[2] ? x[1] : '+' + x[1] + ' ' + x[2] + (x[3] ? ' ' + x[3] : '');
-    });
+    };
+
+    if (phoneInput) phoneInput.addEventListener('input', formatPhone);
+    if (forgotPhoneInput) forgotPhoneInput.addEventListener('input', formatPhone);
 
     // Check password strength
     signupPassword.addEventListener('input', () => {
